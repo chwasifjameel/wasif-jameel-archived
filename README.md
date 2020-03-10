0946df0cf9f6ad325ee273d3b5853888883d467f
wasif jameel <wasif.jameel1@gmail.com>
2020-03-10 17:03:47 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+<p align="center">
+ Logo Here
+</p>
+<h1 align="center">
+  Wasif Jameel website
+</h1>
+
+This is a portfolio website. The code is open source so anyone can use it for there own. I have tried to keep it as much independent as possible so people using this have minimum trouble.
---
+++ src/components/layout.js
@@ -0,0 +1,45 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React from "react"
+import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
+
+import Header from "./header"
+import "./layout.css"
+
+const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
+  return (
+    <>
+      <Header siteTitle={data.site.siteMetadata.title} />
+      <div className="container-fluid">
+        <main>{children}</main>
+        <footer>
+          © {new Date().getFullYear()}, Built By
+          {` `}
+          <a href="https://www.github.com/chwasifjameel">Wasif Jameel</a>
+        </footer>
+      </div>
+    </>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
